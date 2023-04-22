252830c93e4c5565fbd6c6390a16f129cdca8673
heinthant <hthant00chk@gmail.com>
2023-04-23 01:30:59 +0630

    Add relations in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :code_snippets, dependent: :destroy 
+  has_many :ratings, dependent: :destroy 
+  has_many :comments, dependent: :destroy 
 end
