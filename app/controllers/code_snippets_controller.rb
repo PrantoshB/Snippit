23af339f3c4f66b38169deebb03cfaafb1e7f545
heinthant <hthant00chk@gmail.com>
2023-04-24 02:08:29 +0630

    Style layout for pages

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -1,47 +1,47 @@
 class CodeSnippetsController < ApplicationController
-    def index 
-        @code_snippets = CodeSnippet.all 
+  def index
+    @code_snippets = CodeSnippet.all
+  end
+
+  def show
+    @code_snippet = CodeSnippet.find(params[:id])
+  end
+
+  def new
+    @code_snippet = CodeSnippet.new
+  end
+
+  def create
+    @code_snippet = CodeSnippet.new(snippet_params)
+    @code_snippet.user = current_user
+    if @code_snippet.save
+      flash[:success] = 'Snippet uploaded successfully!'
+      redirect_to code_snippet_path(@code_snippet)
+    else
+      flash[:error] = 'Failed to upload snippet! Please try again'
+      render :new
     end
+  end
 
-    def show 
-        @code_snippet = CodeSnippet.find(params[:id])
+  def edit
+    @code_snippet = CodeSnippet.find(params[:id])
+  end
+
+  def update
+    @code_snippet = CodeSnippet.find(params[:id])
+
+    if @code_snippet.update(snippet_params)
+      flash[:success] = 'Snippet updated successfully!'
+      redirect_to code_snippet_path(@code_snippet)
+    else
+      flash[:error] = 'Failed to update snippet! Please try again'
+      render :edit
     end
+  end
 
-    def new 
-        @code_snippet = CodeSnippet.new 
-    end
+  private
 
-    def create
-        @code_snippet = CodeSnippet.new(snippet_params)
-        @code_snippet.user = current_user
-        if @code_snippet.save
-            flash[:success] = "Snippet uploaded successfully!"
-            redirect_to code_snippet_path(@code_snippet)
-        else
-            flash[:error] = 'Failed to upload snippet! Please try again'
-            render :new 
-        end
-    end
-
-    def edit 
-        @code_snippet = CodeSnippet.find(params[:id])
-    end
-
-    def update
-        @code_snippet = CodeSnippet.find(params[:id])
-
-        if @code_snippet.update(snippet_params)
-            flash[:success] = "Snippet updated successfully!"
-            redirect_to code_snippet_path(@code_snippet)
-        else
-            flash[:error] = 'Failed to update snippet! Please try again'
-            render :edit 
-        end
-    end
-
-    private 
-
-    def snippet_params
-        params.require(:code_snippet).permit(:title, :description, :code, :language)
-    end
+  def snippet_params
+    params.require(:code_snippet).permit(:title, :description, :code, :language)
+  end
 end
