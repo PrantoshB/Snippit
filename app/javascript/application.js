2c01c27cf2c96428332f11a92086e15494ac74c0
hein thant <hthant00chk@gmail.com>
2023-07-06 17:25:24 +0630

    Add button to copy link

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,25 @@ import 'controllers';
 
 document.addEventListener('turbo:load', () => {
   Prism.highlightAll();
+  // Focusing the comment form
   const commentBtn = document.querySelector('button[data-focus-comment]');
   const commentTextarea = document.querySelector('#comment-textarea');
 
   commentBtn.addEventListener('click', () => {
     commentTextarea.focus();
+  });
+
+  // Copy to clipboard
+  const copyBtn = document.querySelector('button[data-copy-link]');
+  copyBtn.addEventListener('click', () => {
+    const link = window.location.href;
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        console.log(link);
+      })
+      .catch((error) => {
+        console.error('Failed to copy link:', error);
+      });
   });
 });
