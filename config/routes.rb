981b9caad796c538bfdeb4f16f94a0c0d4e94085
PrantoshB <pbiswas1007@gmail.com>
2023-07-17 23:24:45 +0530

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # root "public#homepage"
-
   namespace :api do 
     namespace :v1 do 
       resources :code_snippets
+      resources :contributors
     end
   end
-
-  # resources :code_snippets, path: 'snippets' do 
-  #   resources :stars, only: [:create, :destroy]
-  #   resources :bookmarks, only: [:create, :destroy]
-  #   resources :ratings, only: [:new, :create, :destroy, :edit, :update]
-  #   resources :comments, only: [:create, :destroy, :edit, :update]
-  # end
 
   resources :contributors do
     resources :code_snippets, path: 'snippets', only: [:index]
