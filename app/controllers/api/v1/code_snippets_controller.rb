3df0d7dd3d062f6cc4a49d79b5dca79e19d31107
hein thant <hthant00chk@gmail.com>
2023-07-17 15:28:49 +0630

    Transform actions for snippet

--- app/controllers/api/v1/code_snippets_controller.rb
+++ app/controllers/api/v1/code_snippets_controller.rb
@@ -11,45 +11,35 @@ class Api::V1::CodeSnippetsController < ApplicationController
     render json: @code_snippet
   end
 
-  def new
-    # @code_snippet = CodeSnippet.new
-  end
-
   def create
-    # @code_snippet = CodeSnippet.new(snippet_params)
-    # @code_snippet.user = current_user
-    # if @code_snippet.save
-    #   flash[:success] = 'Snippet uploaded successfully!'
-    #   redirect_to code_snippet_path(@code_snippet)
-    # else
-    #   flash[:error] = 'Failed to upload snippet! Please try again'
-    #   redirect_to new_code_snippet_path
-    # end
-  end
-
-  def edit
-    # @code_snippet = CodeSnippet.find(params[:id])
+    @code_snippet = CodeSnippet.new(snippet_params)
+    @code_snippet.user = current_user
+    if @code_snippet.save
+      render json: { success: 'Snippet uploaded successfully!', code_snippet: @code_snippet }, status: :created
+    else
+      render json: { error: 'Failed to upload snippet! Please try again' }, status: :unprocessable_entity
+    end
   end
 
   def update
-    # @code_snippet = CodeSnippet.find(params[:id])
+    @code_snippet = CodeSnippet.find(params[:id])
 
-    # if @code_snippet.update(snippet_params)
-    #   flash[:success] = 'Snippet has been uploaded.'
-    #   redirect_to code_snippet_path(@code_snippet)
-    # else
-    #   errors = @code_snippet.errors.full_messages
-    #   flash[:error] = "#{errors}. Please try again."
-    #   render :edit
-    # end
+    if @code_snippet.update(snippet_params)
+      render json: { success: 'Snippet has been updated.', code_snippet: @code_snippet }
+    else
+      render json: { error: @code_snippet.errors.full_messages.join('. ') }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # @code_snippet = CodeSnippet.find(params[:id])
-    # @code_snippet.destroy
+    @code_snippet = CodeSnippet.find(params[:id])
+    @code_snippet.destroy
 
-    # flash[:success] = 'Your snippet has been removed.'
-    # redirect_to code_snippets_path
+    if @code_snippet.destroy
+      render json: { success: 'Your snippet has been removed.' }
+    else
+      render json: { error: 'Failed to remove the snippet.' }, status: :unprocessable_entity
+    end
   end
 
   private
